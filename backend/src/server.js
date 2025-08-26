235df9163e7e668a087a5f98b632fea4a721ce5a
BOB <christian.salapate@keitri.com>
2025-08-26 15:53:51 +0800

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,15 @@ import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
+import path from "path";
 
 import cors from 'cors';
 
-
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 
 //middleware to parse JSON bodies 
@@ -19,13 +21,24 @@ app.use(cors({
 app.use(express.json());
 app.use(rateLimiter);
 
+if (process.env.NODE_ENV !== "production") {
+  app.use((req, res, next) => {
+    console.log("Request received:", req.method, req.url);
+    next();
+  })
+}
 
-app.use((req, res, next) => {
-  console.log("Request received:", req.method, req.url);
-  next();
-})
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
