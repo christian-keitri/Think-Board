123e85d8c417cd3f2e094cb905f26a275fbded5b
BOB <christian.salapate@keitri.com>
2025-08-28 09:45:34 +0800

    imported frontend to backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,16 @@
-import dotenv from "dotenv";
-dotenv.config();
-
+// server.ts (CommonJS)
 import path from "path";
+import express from "express";
 import { connectToDatabase } from "./db/db";
-import app from "./app"; // ESModule import
+import app from "./app";
 
 const PORT = process.env.PORT || 5001;
+
+// __dirname works natively
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
 const startServer = async () => {
   try {
